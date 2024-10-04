ea25406d38f98ef0cba092db339512bed0db4b16
toma425 <fukuyamatoma@icloud.com>
2024-10-04 06:04:42 +0000

    10_4

---
+++ 10_3/main.js
@@ -0,0 +1,15 @@
+//クリックされたら動作する
+
+ function errorInput(){
+    console.log(document.getElementById("month").value)
+    console.log(document.getElementById("day").value)
+
+    const month = document.getElementById("month").value;
+    const day = document.getElementById("day").value;
+
+    console.log("month:",month,"day:",day)
+    
+ }
+ 
+ //document.getElementById("month")はタグを確認するだけ
+ //valueはそのタグの値を示す
