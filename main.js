6f70c83aadbb20fc4902c18367cbc761fc8bbd88
toma425 <fukuyamatoma@icloud.com>
2025-01-16 05:21:31 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-//function ClickCounterGame(){
+function ClickCounterGame(){
     let count = 0;
     const gameContainer = document.getElementById("game-container");
 
     let button1 = document.createElement("button");
+
+    let button2 = document.createElement("button");
+
     button1.textContent = "+1ボタン";
+    button2.textContent = "+10ボタン";
 
     let counter = document.createElement("p");
     counter.textContent = count;
@@ -13,7 +17,18 @@
         counter.textContent = count;
     })
 
+    button2.addEventListener("click",function(){
+        count + 10;
+        counter.textContent = count;
+    })
+
+    counter.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })
+
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
     gameContainer.appendChild(counter);
-//}
+}
 ClickCounterGame();
