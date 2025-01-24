da18fb7c803cf6cf7854d01f9b9af977858c35d3
toma425 <fukuyamatoma@icloud.com>
2025-01-24 06:17:13 +0000

    判定ボタンの追加

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-function ClickCounterGame(){
-    let count = 0;
-    const gameContainer = document.getElementById("game-container");
+// function ClickCounterGame(){
+//     let count = 0;
+//     const gameContainer = document.getElementById("game-container");
 
-    let button1 = document.createElement("button");
+//     let button1 = document.createElement("button");
 
-    let button2 = document.createElement("button");
+//     let button2 = document.createElement("button");
 
-    let button3 = document.createElement("button");
+//     let button3 = document.createElement("button");
 
-    button1.textContent = "+1ボタン";
-    button2.textContent = "+10ボタン";
-    button3.textContent = "リセット";
+//     button1.textContent = "+1ボタン";
+//     button2.textContent = "+10ボタン";
+//     button3.textContent = "リセット";
 
-    let counter = document.createElement("p");
-    counter.textContent = count;
+//     let counter = document.createElement("p");
+//     counter.textContent = count;
 
-    button1.addEventListener("click",function(){
-        if(count < 100){count++;}
-        counter.textContent = count;
-    })
+//     button1.addEventListener("click",function(){
+//         if(count < 100){count++;}
+//         counter.textContent = count;
+//     })
 
-    button2.addEventListener("click",function(){
-        if(count <= 90){ count = count + 10;}
-        counter.textContent = count;
-    })
+//     button2.addEventListener("click",function(){
+//         if(count <= 90){ count = count + 10;}
+//         counter.textContent = count;
+//     })
 
-    button3.addEventListener("click",function(){
-        count = 0;
-        counter.textContent = count;
-    })
+//     button3.addEventListener("click",function(){
+//         count = 0;
+//         counter.textContent = count;
+//     })
 
-    gameContainer.appendChild(button1);
-    gameContainer.appendChild(button2);
-    gameContainer.appendChild(button3);
-    gameContainer.appendChild(counter);
-}
-ClickCounterGame();
+//     gameContainer.appendChild(button1);
+//     gameContainer.appendChild(button2);
+//     gameContainer.appendChild(button3);
+//     gameContainer.appendChild(counter);
+// }
+// ClickCounterGame();
 
 function starNumberGuessGame(){
     const gameContainer = document.getElementById("game-container");
@@ -47,6 +47,21 @@ function starNumberGuessGame(){
     input.placeholder = "好きな数字を入力してください (1 ~ 100)"
     gameContainer.appendChild(message);
     gameContainer.appendChild(input);
+
+    let button = document.createElement("button");
+    button.textContent = "確認"
+
+    button.addEventListener("click",function(){
+        const val = parseInt(input.value);
+        if(ramdomNumber == val){
+            message.textContent = "正解"
+        }else if (ramdomNumber <  val){
+            message.textContent = "大きい"    
+        }else{
+            message.textContent = "小さい"
+        }
+    })
+    gameContainer.appendChild(button);
 }
 
 starNumberGuessGame();
